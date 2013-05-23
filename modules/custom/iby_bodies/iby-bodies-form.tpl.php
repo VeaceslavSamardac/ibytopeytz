9e1e6a21f01fdaacb5314e78afdd17282f9a0d2a
vs <vs@inleadmedia.com>
2013-05-23 10:50:09 +0300

    Cleared iby_bodies moduule.

--- modules/custom/iby_bodies/iby-bodies-form.tpl.php
+++
@@ -1,8 +0,0 @@
-<div>
-  <h2><?php print t('Reclaim your account via email');?></h2>
-  <div class="description"><?php print t('We have moved the content to innovation by you. You can reaclaim your account by entering the email you used on Stoma-Innovation.com') ?><br />
-  <?php print t('We will send you an e-mail with your new login') ?></div>
-</div>
-<?php  
-  print drupal_render_children($form);
-?>
